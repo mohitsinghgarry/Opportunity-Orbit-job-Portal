d360236fcee9e87fef992221f79fc5525b6bc136
Mohit Singh <mohitsinghx4@gmail.com>
2025-01-05 16:20:26 +0530

    minor changes

--- Frontend_codebase/vite.config.js
+++ Frontend_codebase/vite.config.js
@@ -3,8 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'Frontend_codebase/dist', // Explicitly set the output directory to 'dist' inside Frontend_codebase
-    chunkSizeWarningLimit: 1000,
-  }
 })
